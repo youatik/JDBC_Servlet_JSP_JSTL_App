0178d416d22aa86c845ed50f5b22fab47d81ad87
youatik <112843400+youatik@users.noreply.github.com>
2023-06-05 16:34:33 -0400

    First commit

---
+++ src/main/java/dao/LibraryDAOInterface.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dao;
+import models.Library;
+/**
+ *
+ * @author youatik
+ */
+import java.util.List;
+
+public interface LibraryDAOInterface {
+    void addLibrary(Library library);
+    
+    void updateLibrary(Library library);
+    
+    void deleteLibrary(long eanIsbn13);
+    
+    Library getLibraryById(long eanIsbn13);
+    
+    List<Library> getAllLibraries();
+}
