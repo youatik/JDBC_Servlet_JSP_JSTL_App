f9ae1fe5d4f15e3e4f6f6c464500eccc9a7cd4a3
youatik <112843400+youatik@users.noreply.github.com>
2023-06-05 16:34:19 -0400

    First commit

---
+++ src/main/java/models/UserAuthentication.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author youatik
+ */
+public class UserAuthentication {
+    private int clientId;
+    private String username;
+    private String password;
+
+    public UserAuthentication() {
+        // Default constructor
+    }
+
+    public UserAuthentication(int clientId, String username, String password) {
+        this.clientId = clientId;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
